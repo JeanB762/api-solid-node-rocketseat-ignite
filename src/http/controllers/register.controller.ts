b13e4d9752fc807fdc0ac9bec5954db6e01a2ccb
Jean Borges <jeanborges946@gmail.com>
2023-05-04 16:47:18 -0300

    feature: Add register use case, and add routes file

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -1,7 +1,6 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
-import { hash } from 'bcryptjs';
-import { prisma } from '@/lib/prisma';
 import { z } from 'zod';
+import { registerUseCase } from '@/use-cases/register.usecase';
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -12,21 +11,10 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body);
 
-  const emailAlreadyRegistered = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  });
-
-  if (emailAlreadyRegistered) {
+  try {
+    await registerUseCase({ name, email, password });
+  } catch (err) {
     return reply.status(409).send();
   }
-
-  const password_hash = await hash(password, 6);
-
-  await prisma.user.create({
-    data: { name, email, password_hash },
-  });
-
   reply.status(201).send();
 }
