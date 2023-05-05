105fbc5ceabb394f5822a9477728b11dd070e2cc
Jean Borges <jeanborges946@gmail.com>
2023-05-05 15:15:44 -0300

    feature: Error handling

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -2,6 +2,7 @@ import { FastifyRequest, FastifyReply } from 'fastify';
 import { z } from 'zod';
 import { RegisterUseCase } from '@/use-cases/register.usecase';
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-user-repositories';
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error';
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -18,7 +19,10 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     await registerUseCase.execute({ name, email, password });
   } catch (err) {
-    return reply.status(409).send();
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ error: err.message });
+    }
+    throw err;
   }
   reply.status(201).send();
 }
